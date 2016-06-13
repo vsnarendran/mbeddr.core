1e8541595e63cce4575943b2102128a18cf6e7e7
Domenik Pavletic <Domenik@Pavletic.com>
2016-06-13 17:29:49 +0200

    setup a multi-project build with gradle + stared extracting build scripts from Teamcity

---
+++ build/com.mbeddr.analyses.cbmc/build-cbmc-linux.sh
@@ -0,0 +1,8 @@
+make minisat2-download
+make -j12
+mkdir cbmc-linux
+cp src/cbmc/cbmc cbmc-linux
+cp src/goto-cc/goto-cc cbmc-linux
+cp src/goto-instrument/goto-instrument cbmc-linux
+cp LICENSE cbmc-linux
+tar -zcvf cbmc-linux.tar.gz -C cbmc-linux .
