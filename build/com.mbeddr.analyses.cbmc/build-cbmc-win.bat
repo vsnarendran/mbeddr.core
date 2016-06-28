8efbd35a163f70f46c6b3bdc1e6042b6e8d6670e
Domenik Pavletic <Domenik@Pavletic.com>
2016-06-28 14:32:27 +0200

    gradle: fixed paths of cbmc build

--- build/com.mbeddr.analyses.cbmc/build-cbmc-win.bat
+++ build/com.mbeddr.analyses.cbmc/build-cbmc-win.bat
@@ -1,8 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-
 c:\cygwin64\bin\sh.exe -c "wget http://ftp.debian.org/debian/pool/main/m/minisat2/minisat2_2.2.1.orig.tar.gz; tar xfz minisat2_2.2.1.orig.tar.gz; mv minisat2-2.2.1 minisat-2.2.1; cd minisat-2.2.1; patch -p1 < ../scripts/minisat-2.2.1-patch;"
 cd src
-
 c:\cygwin64\bin\sh.exe -c "sed -e s/AUTO/MSVC/g config.inc > config.inc.1" 
 mv config.inc.1 config.inc
 make -j12
