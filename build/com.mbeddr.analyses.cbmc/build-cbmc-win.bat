502bb7d33cabb453428995154beafd81e095c099
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-23 14:08:04 +0200

    build: further path corrections ...

--- build/com.mbeddr.analyses.cbmc/build-cbmc-win.bat
+++ build/com.mbeddr.analyses.cbmc/build-cbmc-win.bat
@@ -1,5 +1,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 
+cd ..
+
 c:\cygwin64\bin\sh.exe -c "wget http://ftp.debian.org/debian/pool/main/m/minisat2/minisat2_2.2.1.orig.tar.gz; tar xzf minisat2_2.2.1.orig.tar.gz; mv minisat2-2.2.1 minisat-2.2.1; cd minisat-2.2.1; patch -p1 < ../scripts/minisat-2.2.1-patch;"
 
 cd src
