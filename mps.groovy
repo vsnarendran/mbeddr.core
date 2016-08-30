c5985071688750a324d7845d41019c7569ee27f6
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-30 17:29:52 +0200

    build: simplified mps build step

--- mps.groovy
+++ mps.groovy
@@ -3,25 +3,12 @@ def buildMps() {
 	echo "Running 'MPS' build..."
 	timestamps {
 		def gradleOpts ='--no-daemon --info'
-		def customEnv = setupEnvironment()
-
-		withEnv(customEnv) {
-			stage 'Download MPS'
-				sh "./gradlew ${gradleOpts} -b build.gradle getMPS"
-                withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'mbeddr-ci',
-                    usernameVariable: 'nexusUsername', passwordVariable: 'nexusPassword']])
-                {
-					sh "./gradlew ${gradleOpts} -b build.gradle publishMpsPublicationToMavenRepository"
-				}
+		withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'mbeddr-ci',
+						  usernameVariable: 'nexusUsername', passwordVariable: 'nexusPassword']]) {
+			sh "./gradlew ${gradleOpts} -b build.gradle getMPS"
+			sh "./gradlew ${gradleOpts} -b build.gradle publishMpsPublicationToMavenRepository"
 		}
 	}
-}
-
-def setupEnvironment() {
-	def javaHome = tool(name: 'JDK 8')
-	def antHome = tool(name: 'Ant 1.9')
-	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
-	return customEnv
 }
 
 return this;
