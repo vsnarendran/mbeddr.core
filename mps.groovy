f1b56cc0c59f6ff3074d2b420bdf364d521e1307
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-30 16:29:44 +0200

    build: added required setupEnv to mps.groovy

--- mps.groovy
+++ mps.groovy
@@ -1,3 +1,4 @@
+
 def buildMps() {
 	echo "Running 'MPS' build..."
 	timestamps {
@@ -9,4 +10,11 @@ def buildMps() {
 				sh "./gradlew ${gradleOpts} -b build.gradle publishMpsPublicationToMavenRepository"
 		}
 	}
+}
+
+def setupEnvironment() {
+	def javaHome = tool(name: 'JDK 8')
+	def antHome = tool(name: 'Ant 1.9')
+	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
+	return customEnv
 }
