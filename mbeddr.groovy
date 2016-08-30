20dad543c15f7d5fdd7ed78c4815b691045b8ada
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-30 17:54:03 +0200

    re-added SetupEnv in mbeddr.groovy

--- mbeddr.groovy
+++ mbeddr.groovy
@@ -1,5 +1,4 @@
 #!groovy
-evaluate(new File("util.groovy"))
 def buildMbeddr() {
   timestamps {
 	  def gradleOpts ='--no-daemon --info'
@@ -147,5 +146,11 @@ def checkoutMbeddr() {
 		])
 }
 
+def static setupEnvironment() {
+	def javaHome = tool(name: 'JDK 8')
+	def antHome = tool(name: 'Ant 1.9')
+	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
+	return customEnv
+}
 
 return this;
