a7cd91c324dc803f4453054b98e0ba3d9c27abec
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-30 14:17:46 +0200

    reverted changes to nightly.groovy

--- nightly.groovy
+++ nightly.groovy
@@ -12,7 +12,7 @@ def buildNightly() {
 	}
 }
 
-def static setupEnvironment() {
+def setupEnvironment() {
 	def javaHome = tool(name: 'JDK 8')
 	def antHome = tool(name: 'Ant 1.9')
 	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
