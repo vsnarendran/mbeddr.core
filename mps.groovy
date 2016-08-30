044b3334e4299a1a4e3c8d8d799eed7b831943ec
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-30 15:00:05 +0200

    build: pubhlish rules for mps

--- mps.groovy
+++ mps.groovy
@@ -1,3 +1,12 @@
 def buildMps() {
 	echo "Running 'MPS' build..."
+	timestamps {
+		def gradleOpts ='--no-daemon --info'
+		def customEnv = setupEnvironment()
+
+		withEnv(customEnv) {
+			stage 'Download and publish MPS'
+				sh "./gradlew ${gradleOpts} -b build.gradle publishMpsPublicationToMavenRepository"
+		}
+	}
 }
