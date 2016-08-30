3cf96f97aa8d6ddebd295a0cd69a9d4df9f0b267
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-30 17:57:09 +0200

    removed faulty static keyword from setupEnv

--- mbeddr.groovy
+++ mbeddr.groovy
@@ -146,7 +146,7 @@ def checkoutMbeddr() {
 		])
 }
 
-def static setupEnvironment() {
+def setupEnvironment() {
 	def javaHome = tool(name: 'JDK 8')
 	def antHome = tool(name: 'Ant 1.9')
 	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
