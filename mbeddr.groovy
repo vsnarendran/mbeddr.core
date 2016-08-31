9c41ae3d60677a3b9efe8ddfff1a7e7927b5ca54
Domenik Pavletic <Domenik@Pavletic.com>
2016-08-31 14:48:08 +0200

    build: introduced a git clean in mbeddr.core build

--- mbeddr.groovy
+++ mbeddr.groovy
@@ -1,9 +1,9 @@
 #!groovy
 def buildMbeddr() {
   timestamps {
+	  gitClean()
 	  def gradleOpts ='--no-daemon --info'
 	  def customEnv = setupEnvironment()
-
 	  withEnv(customEnv) {
 	    stage 'Generate Build Scripts'
 	        sh "./gradlew ${gradleOpts} -b build.gradle build_allScripts"
@@ -151,6 +151,35 @@ def setupEnvironment() {
 	def antHome = tool(name: 'Ant 1.9')
 	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
 	return customEnv
+}
+
+/**
+ * Clean a Git project workspace.
+ * Uses 'git clean' if there is a repository found.
+ * Uses Pipeline 'deleteDir()' function if no .git directory is found.
+ */
+def gitClean() {
+    timeout(time: 60, unit: 'SECONDS') {
+        if (fileExists('.git')) {
+            echo 'Found Git repository: using Git to clean the tree.'
+            // The sequence of reset --hard and clean -fdx first
+            // in the root and then using submodule foreach
+            // is based on how the Jenkins Git SCM clean before checkout
+            // feature works.
+            sh 'git reset --hard'
+            // Note: -e is necessary to exclude the temp directory
+            // .jenkins-XXXXX in the workspace where Pipeline puts the
+            // batch file for the 'bat' command.
+            sh 'git clean -ffdx -e ".jenkins-*/"'
+            sh 'git submodule foreach --recursive git reset --hard'
+            sh 'git submodule foreach --recursive git clean -ffdx'
+        }
+        else
+        {
+            echo 'No Git repository found: using deleteDir() to wipe clean'
+            deleteDir()
+        }
+    }
 }
 
 return this;
