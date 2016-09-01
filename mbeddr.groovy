f314fa4b9fe9d61e5e0a4f9ab25d3523595933a4
Domenik Pavletic <Domenik@Pavletic.com>
2016-09-01 13:25:42 +0200

    build: added ANT_HOME to Jenkin's environment path

--- mbeddr.groovy
+++ mbeddr.groovy
@@ -162,7 +162,7 @@ def checkoutMbeddr() {
 def setupEnvironment() {
 	def javaHome = tool(name: 'JDK 8')
 	def antHome = tool(name: 'Ant 1.9')
-	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
+	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
 	return customEnv
 }
 
