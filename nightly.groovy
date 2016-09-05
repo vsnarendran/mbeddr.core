500df5f5edcc2697c4a5a1310fefab3ba86e3616
Domenik Pavletic <Domenik@Pavletic.com>
2016-09-05 11:37:28 +0200

    reverted changes to nightly.groovy

--- nightly.groovy
+++ nightly.groovy
@@ -1,18 +1,19 @@
-#!groovy
 def buildNightly() {
-	timestamps {
+    echo "Running 'Nightly' build..."
+/*	timestamps {
 		def gradleOpts ='--no-daemon --info'
 		def customEnv = setupEnvironment()
 		withEnv(customEnv) {
 			stage 'Build RCP'
 				sh "./gradlew ${gradleOpts} -b build.gradle build_mbeddrRCPDistributuion"
 		}
-	}
+	}*/
 }
 
+/*
 def setupEnvironment() {
 	def javaHome = tool(name: 'JDK 8')
 	def antHome = tool(name: 'Ant 1.9')
 	def customEnv = ["PATH+JDK=${javaHome}/bin", "PATH+ANT_HOME=${antHome}/bin", "JAVA_HOME=${javaHome}"]
 	return customEnv
-}
+}*/
